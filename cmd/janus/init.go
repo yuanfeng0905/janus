6eb4037c0f1de2a0b6e01670e847b012029ab807
Vladimir Garvardt <vgarvardt@gmail.com>
2017-03-17 16:51:57 +0100

    Replaced stats pkg with stats-go lib

--- cmd/janus/init.go
+++ cmd/janus/init.go
@@ -8,14 +8,12 @@ import (
 	"github.com/bshuster-repo/logrus-logstash-hook"
 	"github.com/hellofresh/janus/pkg/config"
 	"github.com/hellofresh/janus/pkg/store"
-	statsd "gopkg.in/alexcesaro/statsd.v2"
 )
 
 var (
 	err          error
 	globalConfig *config.Specification
 	storage      store.Store
-	statsdClient *statsd.Client
 )
 
 // initializes the global configuration
@@ -46,32 +44,5 @@ func init() {
 	storage, err = store.Build(globalConfig.StorageDSN)
 	if nil != err {
 		log.Panic(err)
-	}
-}
-
-// initializes new StatsD client if it enabled
-func init() {
-	var options []statsd.Option
-	statsdConfig := globalConfig.Statsd
-
-	log.Debugf("Trying to connect to statsd instance: %s", statsdConfig.DSN)
-	if statsdConfig.IsEnabled() {
-		log.Debug("Statsd DSN not provided, client will be muted")
-		options = append(options, statsd.Mute(true))
-	} else {
-		options = append(options, statsd.Address(statsdConfig.DSN))
-	}
-
-	if statsdConfig.HasPrefix() {
-		options = append(options, statsd.Prefix(statsdConfig.Prefix))
-	}
-
-	statsdClient, err = statsd.New(options...)
-	if err != nil {
-		log.WithError(err).
-			WithFields(log.Fields{
-				"dsn":    statsdConfig.DSN,
-				"prefix": statsdConfig.Prefix,
-			}).Warning("An error occurred while connecting to StatsD. Client will be muted.")
 	}
 }
